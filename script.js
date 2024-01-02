5f2a6dcddf22b01b8815bdab639caabb8af6ca0e
jarmar123 <jason.marlette@gmail.com>
2024-01-02 16:29:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ $(document).ready(() => {
   let userName = null;
   let level = 0;
   let levelContainer = $('.game-stats .level span.val');
-  let lifes = 3;
+  let lifes = 1;
   let lifesContainer = $('.game-stats .lifes span.val');
   let gameBoard = $('svg #game-board');
   let gameBoardOption = $('svg #game-board .option');
-  let colorArray = ['#1abc9c', '#16a085', '#27ae60', '#2ecc71', '#90C695', '#26A65B', '#66CC99', '#C8F7C5', '#2980b9', '#3498db', '#59ABE3', '#81CFE0', '#67809F', '#4B77BE', '#8e44ad', '#9b59b6', '#BF55EC', '#9B59B6', '#947CB0', '#2c3e50', '#34495e', '#E9D460', '#FDE3A7', '#F5AB35', '#F2784B', '#F9BF3B', '#f1c40f', '#f39c12', '#e67e22', '#e74c3c', '#d35400', '#c0392b', '#EC644B', '#D24D57', '#C0392B', '#DB0A5B', '#D2527F'];
+  let colorArray = ['#5C6A85','#C8DEE2','#F0F1EA','#958896','#8D8C7F','#373D3F','#B66340','#F7C8B7','#EFDFCC','#ECDF9B'];
+  let colorArray2 = ['#5B6D92','#CBDDE2','#EFEEE5','#9B8E9C','#8B8A7B','#364042','#C06A46','#F4C3B2','#F0E2D2','#EFD99A'];  
   let currentBoard = null;
   let playNow = $('button.play-now');
   let startScreem = $('.start-screen');
@@ -15,6 +16,8 @@ $(document).ready(() => {
   let gameScreen = $('.game');
   let message = $('.message');
   let gameOverScreen = $('.game-over');
+  let gameOverWinScreen = $('.game-over-win');
+  let gameOverLoseScreen = $('.game-over-lose');										
   let replay = $('.replay');
 
   function rand(min, max) {
@@ -37,6 +40,25 @@ $(document).ready(() => {
     }
     return rgb;
   }
+  
+  function ColorLuminance2(index) {
+    // validate hex string
+    // hex = String(hex).replace(/[^0-9a-f]/gi, '');
+    // if (hex.length < 6) {
+      // hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    // }
+    // lum = lum || 0;
+    // // convert to decimal and change luminosity
+    // let rgb = "#",c,i;
+    // for (i = 0; i < 3; i++) {
+      // c = parseInt(hex.substr(i * 2, 2), 16);
+      // c = Math.round(Math.min(Math.max(0, c + c * lum), 255)).toString(16);
+      // rgb += ("00" + c).substr(c.length);
+    // }
+    // return rgb;
+	
+	return colorArray2[index];
+  }
 
   function Board() {
     this.nOptions = gameBoardOption.length;
@@ -44,7 +66,8 @@ $(document).ready(() => {
     this.selectedColor = rand(0, colorArray.length);
     this.dificulty = rand(2, 12) / 100;
     this.primaryColor = colorArray[this.selectedColor];
-    this.secondaryColor = ColorLuminance(colorArray[this.selectedColor], this.dificulty);
+    //this.secondaryColor = ColorLuminance(colorArray[this.selectedColor], this.dificulty);
+    this.secondaryColor = ColorLuminance2(this.selectedColor);
   }
 
   function gameOver() {
@@ -56,13 +79,32 @@ $(document).ready(() => {
     gameOverScreen.find('.score').html('<span>' + userName + '</span><span><i class="fa fa-star-o" aria-hidden="true"></i>' + level + '</span>');
   }
 
+  function gameOverWin() {
+    if (gameScreen.hasClass('open')) {
+      gameScreen.removeClass('open');
+    }
+    gameOverWinScreen.addClass('open');
+    gameOverWinScreen.find('.score').html('<span>' + userName + '</span><span><i class="fa fa-star-o" aria-hidden="true"></i>' + level + '</span>');
+  }
+
+  function gameOverLose() {
+    if (gameScreen.hasClass('open')) {
+      gameScreen.removeClass('open');
+    }
+    gameOverLoseScreen.addClass('open');
+    gameOverLoseScreen.find('.score').html('<span>' + userName + '</span><span><i class="fa fa-star-o" aria-hidden="true"></i>' + level + '</span>');
+  }
   function updateStats() {
 
     levelContainer.html(level);
     lifesContainer.html(lifes);
 
+    if (level === 10) {
+      gameOverWin();
+    }
+    else
     if (lifes === 0) {
-      gameOver();
+      gameOverLose();
     }
 
   }
@@ -107,6 +149,11 @@ $(document).ready(() => {
     } else {
       lifes--;
     }
+	
+	// if(level == 10){
+				// alert("Success! You have received a BM promocode!");
+				// gameOver();
+	  // }
 
     newBoard();
 
@@ -120,7 +167,7 @@ $(document).ready(() => {
     } else {
       userName = 'Player';
     }
-    lifes = 3;
+    lifes = 1;
     level = 0;
     gameScreen.find('.user-name').text(userName);
     newBoard();
@@ -141,7 +188,15 @@ $(document).ready(() => {
   });
 
   replay.bind('click', function () {
-    gameOverScreen.removeClass('open');
+    if (gameOverScreen.hasClass('open')) {
+      gameOverScreen.removeClass('open');
+    }
+    if (gameOverWinScreen.hasClass('open')) {
+      gameOverWinScreen.removeClass('open');
+    }
+    if (gameOverLoseScreen.hasClass('open')) {
+      gameOverLoseScreen.removeClass('open');
+    } 
     startScreem.addClass('open');
   });
 
