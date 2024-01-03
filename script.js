e178fe3f1c959853d2f7331e60a70db022dddbdb
jarmar123 <jason.marlette@gmail.com>
2024-01-03 10:33:30 -0500

    Update script.js
    randomizer

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ $(document).ready(() => {
   let lifesContainer = $('.game-stats .lifes span.val');
   let gameBoard = $('svg #game-board');
   let gameBoardOption = $('svg #game-board .option');
-	// updated color arrays 01/03/2024.10:02
   let colorArray = ['#696D86','#CEDDDF','#EEECE1','#988B90','#8D887A','#2F3E3E','#B86D52','#FDC8B6','#EFDFCC','#EEE3A4'];
   let colorArray2 = ['#5B6D92','#CBDDE2','#EFEEE5','#9B8E9C','#8B8A7B','#364042','#C06A46','#F4C3B2','#F0E2D2','#EFD99A'];  
+  //let colorArray2 = ['#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF','#FFFFFF'];  
   let currentBoard = null;
   let playNow = $('button.play-now');
   let startScreem = $('.start-screen');
@@ -20,6 +20,7 @@ $(document).ready(() => {
   let gameOverWinScreen = $('.game-over-win');
   let gameOverLoseScreen = $('.game-over-lose');										
   let replay = $('.replay');
+  let randomIndexArr = generateRandomArray(colorArray.length - 1);
 
   function rand(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -41,6 +42,42 @@ $(document).ready(() => {
     }
     return rgb;
   }
+  
+  function generateRandomArray(n) {
+	//var nums = [1,2,3,4,5,6,7,8,9,10],
+	const nums = Array.from({ length: n + 1 }, (_, index) => index);
+	  
+    ranNums = [],
+    i = nums.length,
+    j = 0;
+
+	while (i--) {
+    j = Math.floor(Math.random() * (i+1));
+    ranNums.push(nums[j]);
+	console.log("ranNum: "+nums[j]);
+    nums.splice(j,1);
+	}
+	return ranNums;
+  }
+  // function generateRandomArray(n) {
+	  // if (n < 0) {
+		// console.error("Invalid input: n should be a non-negative number.");
+		// return [];
+	  // }
+
+	  // // Create an array from 0 to n
+	  // const originalArray = Array.from({ length: n + 1 }, (_, index) => index);
+
+	  // // Shuffle the array using the Fisher-Yates algorithm
+	  // for (let i = originalArray.length - 1; i > 0; i--) {
+		// const j = Math.floor(Math.random() * (i + 1));
+		// console.log("j: "+j);
+		// [originalArray[i], originalArray[j]] = [originalArray[j], originalArray[i]];
+	  // }
+
+	  // // Return the first n elements of the shuffled array
+	  // return originalArray.slice(0, n);
+// }
   
   function ColorLuminance2(index) {
     // validate hex string
@@ -66,9 +103,18 @@ $(document).ready(() => {
     this.selectedOption = rand(1, this.nOptions);
     this.selectedColor = rand(0, colorArray.length);
     this.dificulty = rand(2, 12) / 100;
-    this.primaryColor = colorArray[this.selectedColor];
-    //this.secondaryColor = ColorLuminance(colorArray[this.selectedColor], this.dificulty);
-    this.secondaryColor = ColorLuminance2(this.selectedColor);
+	
+	let s = randomIndexArr[0];
+	randomIndexArr.splice(0, 1);
+	
+	this.primaryColor = colorArray[s];
+    this.secondaryColor = ColorLuminance2(s);
+	
+	console.log("primaryColor: "+this.primaryColor);
+	console.log("secondaryColor: "+this.secondaryColor);	
+	
+    // this.primaryColor = colorArray[this.selectedColor];
+    // this.secondaryColor = ColorLuminance2(this.selectedColor);
   }
 
   function gameOver() {
@@ -173,6 +219,8 @@ $(document).ready(() => {
     gameScreen.find('.user-name').text(userName);
     newBoard();
     console.log(userName);
+    console.log("play-now");
+	randomIndexArr = generateRandomArray(colorArray.length - 1);
   });
 
   reset.bind('click', function () {
@@ -189,6 +237,7 @@ $(document).ready(() => {
   });
 
   replay.bind('click', function () {
+	  console.log("replay bind click");
     if (gameOverScreen.hasClass('open')) {
       gameOverScreen.removeClass('open');
     }
